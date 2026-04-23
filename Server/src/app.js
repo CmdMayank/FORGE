fe9955e58322cee1898f09a279685fdf360ba0aa
Mayank <manku2406raksh@gmail.com>
2026-04-24 01:09:02 +0530

    fix cors for production

--- Server/src/app.js
+++ Server/src/app.js
@@ -9,7 +9,7 @@ if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'));
 }
 
-app.use(cors({ origin: 'http://localhost:5173', credentials: true }));
+app.use(cors({ origin: ['http://localhost:5173',"https://forge-eight-mauve.vercel.app"], credentials: true }));
 app.use(express.json());
 app.use('/uploads', express.static('uploads'));
 
